f5b756849c582ca6fec174bbc18c290e6612afce
fjc <feijiuchun@163.com>
2018-06-09 19:09:03 +0800

    平均值

--- src/main/webapp/assets/js/dayGood/index.js
+++ src/main/webapp/assets/js/dayGood/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".companyName").each(function () {
+  $(".companyName").mouseover(function () {
     var id=$(this).attr("id");
     if($('#info' + id).attr("data-content")=="") {
       var htmlValue = "";
@@ -7,21 +7,18 @@ $(document).ready(function () {
         url: "/day/info?companyCode=" + id,
         dataType: "json",
         success: function (result) {
-          if (result.avg != undefined && result.avg != null) {
-            htmlValue += "avg:" + result.avg + ";</br>";
-          }
           if (result.topValue != undefined && result.topValue != null
               && result.topValue.length > 0) {
-            htmlValue += "top:" + result.topValue[0].endPrice + ",date:"
-                + result.topValue[0].date + ";";
+            htmlValue += "top:" + result.topValue[0].endPrice +";";
           }
+          if (result.avg != undefined && result.avg != null) {
+            htmlValue += "avg:" + result.avg;
+          }
+
           $('#info' + id).attr("data-content", htmlValue);
         }
       });
     }
-  });
-
-  $(".companyName").mouseover(function () {
     $('#info' + $(this).attr("id")).popover('show');
   });
   $(".companyName").mouseout(function () {
